c3f5530f523307fc614bef8b927c4a0d0186fdf9
bramabbrhm <abrahamvalentino.t@gmail.com>
2025-05-16 16:04:35 +0700

    edit : href header home

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
     <div class="d-flex align-items-center gap-4">
       <!-- Navigation Links -->
       <div class="d-none d-md-flex gap-4">
-        <a href="#" class="text-white fw-bold text-decoration-none hover-gold">HOME</a>
-        <a href="#" class="text-white fw-bold text-decoration-none hover-gold">MENU</a>
+        <a href="index.php" class="text-white fw-bold text-decoration-none hover-gold">HOME</a>
+        <a href="indexmenu.php" class="text-white fw-bold text-decoration-none hover-gold">MENU</a>
         <a href="#" class="text-white fw-bold text-decoration-none hover-gold">ABOUT</a>
       </div>
       
