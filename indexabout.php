6d7056bbf897445d6468f128337bebee3ec2dc46
mlnayrnhabibi <maulanayorinhabibi@gmail.com>
2025-05-22 14:56:37 +0700

    about

--- indexabout.php
+++ indexabout.php
@@ -85,8 +85,7 @@
           <i class="fas fa-coffee me-2"></i>TENTANG BOERJO
         </h5>
         <p class="mb-0">
-          Warung makan legendaris sejak 1996, menghadirkan berbagai hidangan berkualitas
-          dengan bahan pilihan langsung dari petani lokal.
+          Berdiri sejak tahun 2010, menjadikan Boerjo menjadi salah satu pelopor warung makan di kota ini. Dengan dedikasi tinggi , kami berkomitmen untuk menyajikan makanan yang lezat dan berkualitas
         </p>
       </div>
 
