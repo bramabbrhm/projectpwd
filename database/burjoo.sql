a02434564947d9e078127f45e99ff11e56c8bdfe
bramabbrhm <abrahamvalentino.t@gmail.com>
2025-05-26 14:16:52 +0700

    Update burjoo.sql

--- database/burjoo.sql
+++ database/burjoo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2025 at 06:35 AM
+-- Generation Time: May 26, 2025 at 09:15 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -61,8 +61,8 @@ CREATE TABLE `jml_pesanan` (
 
 INSERT INTO `jml_pesanan` (`id_pesanan`, `no_pesanan`, `id_menu`, `kuantitas`, `subtotal`) VALUES
 (3, 11, 6, 1, 10000),
-(4, 12, 1, 1, 10000),
-(5, 12, 6, 1, 10000);
+(6, 12, 1, 1, 10000),
+(7, 12, 6, 1, 10000);
 
 -- --------------------------------------------------------
 
@@ -99,9 +99,9 @@ CREATE TABLE `pesanan` (
   `waktu_pesan` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `no_meja` int(3) NOT NULL,
   `nama_pelanggan` varchar(100) NOT NULL,
-  `no_telp` int(15) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
   `total_harga` int(11) NOT NULL,
-  `status_pesanan` tinyint(1) NOT NULL,
+  `status_pesanan` enum('Menunggu','Diproses','Selesai','Dibatalkan') NOT NULL DEFAULT 'Menunggu',
   `metode_bayar` varchar(20) NOT NULL,
   `status_bayar` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -111,17 +111,17 @@ CREATE TABLE `pesanan` (
 --
 
 INSERT INTO `pesanan` (`no_pesanan`, `waktu_pesan`, `no_meja`, `nama_pelanggan`, `no_telp`, `total_harga`, `status_pesanan`, `metode_bayar`, `status_bayar`) VALUES
-(1, '0000-00-00 00:00:00', 10, 'jamal', 0, 0, 0, '', 0),
-(2, '0000-00-00 00:00:00', 10, 'harum', 0, 0, 0, '', 0),
-(3, '0000-00-00 00:00:00', 10, 'kezz', 0, 0, 0, '', 0),
-(4, '0000-00-00 00:00:00', 10, 'kezz', 0, 0, 0, '', 0),
-(6, '2025-05-22 09:37:12', 9, 'jamalff', 0, 0, 0, '', 0),
-(7, '2025-05-23 03:29:45', 8, 'hed', 0, 0, 0, '', 0),
-(8, '2025-05-23 04:04:09', 8, 'dsasda', 0, 0, 0, '', 0),
-(9, '2025-05-25 08:07:30', 9, 'reff', 0, 0, 0, '', 0),
-(10, '2025-05-25 09:00:41', 10, 'ghege', 0, 0, 0, '', 0),
-(11, '2025-05-26 02:57:56', 8, 'fafa', 0, 10000, 0, '', 0),
-(12, '2025-05-26 03:52:31', 8, 'ffaf', 0, 20000, 0, '', 0);
+(1, '0000-00-00 00:00:00', 10, 'jamal', '0', 0, '', '', 0),
+(2, '0000-00-00 00:00:00', 10, 'harum', '0', 0, '', '', 0),
+(3, '0000-00-00 00:00:00', 10, 'kezz', '0', 0, '', '', 0),
+(4, '0000-00-00 00:00:00', 10, 'kezz', '0', 0, '', '', 0),
+(6, '2025-05-22 09:37:12', 9, 'jamalff', '0', 0, '', '', 0),
+(7, '2025-05-23 03:29:45', 8, 'hed', '0', 0, '', '', 0),
+(8, '2025-05-23 04:04:09', 8, 'dsasda', '0', 0, '', '', 0),
+(9, '2025-05-25 08:07:30', 9, 'reff', '0', 0, '', '', 0),
+(10, '2025-05-25 09:00:41', 10, 'ghege', '0', 0, '', '', 0),
+(11, '2025-05-26 02:57:56', 8, 'fafa', '0', 10000, '', '', 0),
+(12, '2025-05-26 07:04:05', 8, 'harum', '02147483647', 20000, 'Diproses', 'Tunai', 1);
 
 --
 -- Indexes for dumped tables
@@ -167,7 +167,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `jml_pesanan`
 --
 ALTER TABLE `jml_pesanan`
-  MODIFY `id_pesanan` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_pesanan` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `menu`
