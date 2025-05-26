961e4d725fc0098ee5a848508bd660a98d6b43ea
bramabbrhm <abrahamvalentino.t@gmail.com>
2025-05-26 13:07:36 +0700

    add row status & metode bayar on pesanan table

--- database/burjoo.sql
+++ database/burjoo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2025 at 10:01 AM
+-- Generation Time: May 26, 2025 at 06:35 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,6 +33,14 @@ CREATE TABLE `admin` (
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
+(1, 'wangy123', 'harumganteng'),
+(2, 'wangy123', 'harumganteng');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +55,15 @@ CREATE TABLE `jml_pesanan` (
   `subtotal` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `jml_pesanan`
+--
+
+INSERT INTO `jml_pesanan` (`id_pesanan`, `no_pesanan`, `id_menu`, `kuantitas`, `subtotal`) VALUES
+(3, 11, 6, 1, 10000),
+(4, 12, 1, 1, 10000),
+(5, 12, 6, 1, 10000);
+
 -- --------------------------------------------------------
 
 --
@@ -56,12 +73,20 @@ CREATE TABLE `jml_pesanan` (
 CREATE TABLE `menu` (
   `id_menu` int(3) NOT NULL,
   `tipe` varchar(8) NOT NULL,
-  `nama` int(30) NOT NULL,
+  `nama` varchar(30) NOT NULL,
   `deskripsi` varchar(100) NOT NULL,
   `status_stok` tinyint(1) NOT NULL,
   `harga` int(9) NOT NULL,
   `gambar` varchar(512) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id_menu`, `tipe`, `nama`, `deskripsi`, `status_stok`, `harga`, `gambar`) VALUES
+(1, 'Makanan', 'Nasi Goreng', 'nasi gowrenggg enak ni bosquuu', 1, 10000, 'assets/nasigoreng.jpg'),
+(6, 'Minuman', 'Bubur Kacang Hijau', 'ini bubur ayam ga sih', 1, 10000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqbOi2n9qgT3aXUKHgB3e_4dJzMW7Fcjs5zw&s');
 
 -- --------------------------------------------------------
 
@@ -76,8 +101,27 @@ CREATE TABLE `pesanan` (
   `nama_pelanggan` varchar(100) NOT NULL,
   `no_telp` int(15) NOT NULL,
   `total_harga` int(11) NOT NULL,
-  `status_pesanan` tinyint(1) NOT NULL
+  `status_pesanan` tinyint(1) NOT NULL,
+  `metode_bayar` varchar(20) NOT NULL,
+  `status_bayar` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+INSERT INTO `pesanan` (`no_pesanan`, `waktu_pesan`, `no_meja`, `nama_pelanggan`, `no_telp`, `total_harga`, `status_pesanan`, `metode_bayar`, `status_bayar`) VALUES
+(1, '0000-00-00 00:00:00', 10, 'jamal', 0, 0, 0, '', 0),
+(2, '0000-00-00 00:00:00', 10, 'harum', 0, 0, 0, '', 0),
+(3, '0000-00-00 00:00:00', 10, 'kezz', 0, 0, 0, '', 0),
+(4, '0000-00-00 00:00:00', 10, 'kezz', 0, 0, 0, '', 0),
+(6, '2025-05-22 09:37:12', 9, 'jamalff', 0, 0, 0, '', 0),
+(7, '2025-05-23 03:29:45', 8, 'hed', 0, 0, 0, '', 0),
+(8, '2025-05-23 04:04:09', 8, 'dsasda', 0, 0, 0, '', 0),
+(9, '2025-05-25 08:07:30', 9, 'reff', 0, 0, 0, '', 0),
+(10, '2025-05-25 09:00:41', 10, 'ghege', 0, 0, 0, '', 0),
+(11, '2025-05-26 02:57:56', 8, 'fafa', 0, 10000, 0, '', 0),
+(12, '2025-05-26 03:52:31', 8, 'ffaf', 0, 20000, 0, '', 0);
 
 --
 -- Indexes for dumped tables
@@ -117,25 +161,25 @@ ALTER TABLE `pesanan`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_admin` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `jml_pesanan`
 --
 ALTER TABLE `jml_pesanan`
-  MODIFY `id_pesanan` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pesanan` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id_menu` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_menu` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `pesanan`
 --
 ALTER TABLE `pesanan`
-  MODIFY `no_pesanan` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `no_pesanan` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
